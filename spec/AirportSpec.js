5c184614988a945201c9ff291881f842531318f3
John Ashton <John_ashton@hotmail.com>
2017-02-01 17:12:05 +0000

    Started some capacity additions, no tests though.  Tests for checking error raised when stormy needed too.

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -4,12 +4,16 @@ describe('Airport', function(){
   });
   beforeEach(function() {
     plane = jasmine.createSpyObj('plane', ['land', 'takeoff']);
-  })
+  });
+  beforeEach(function() {
+    weather = jasmine.createSpyObj('weather', ['isStormy']);
+  });
 
 
   describe('when landing it', function(){
     it('stores the plane in the hangar',function(){
       airport.arrive(plane);
+      spyOn(Math, 'random').and.returnValue(0.4);
       expect(airport._hangar).toContain(plane)
       expect(plane.land).toHaveBeenCalled();
     });
@@ -23,6 +27,7 @@ describe('Airport', function(){
 
   describe('when taking off', function(){
     it('removes a plane from the hangar', function(){
+      spyOn(Math, 'random').and.returnValue(0.4);
       airport.depart(plane);
       expect(airport._hangar).not.toContain(plane)
       expect(plane.takeoff).toHaveBeenCalled();
--- src/Airport.js
+++ src/Airport.js
@@ -1,5 +1,6 @@
 function Airport() {
   this._hangar = [];
+  this._maxCapacity = 50;
 
 };
 
@@ -8,11 +9,31 @@ Airport.prototype.land = function(){
 };
 
 Airport.prototype.arrive = function(plane){
-  this._hangar.push(plane)
-  plane.land();
+      weather.isStormy
+  if (weather.isStormy === true) {console.log("Plane can not land")}
+  else {  this._hangar.push(plane)
+          plane.land();
+  };
 };
 
 Airport.prototype.depart = function(plane){
-  this._hangar.pop(plane)
-  plane.takeoff();
+      this._capacityReached
+      weather.isStormy
+  if (weather.isStormy === true) {console.log("Plane can not take off")}
+else {  this._hangar.pop(plane)
+        plane.takeoff();
+};
+
+Airport.prototype.changeCapacity = function(max){
+  this._maxCapacity = max
+};
+
+Airport.prototype._capacityReached = function (){
+  if (this._hangar >= this._maxCapacity)
+      {throw new Error ("Airport is at maximum capacity, can not land")}
+  else { return false
+
+  }
+};
+
 };
